20379fcf2befdfd3ce651cd807018fc692d8aa78
Ralph Moser <ralph@ramosoft.ch>
2015-10-06 14:16:38 +0200

    - view unpublished content module added - admin/editor permission changed - user with title - db updated

--- all/themes/hso_theme/templates/user-profile.tpl.php
+++ all/themes/hso_theme/templates/user-profile.tpl.php
@@ -1,16 +1,20 @@
 <?php
 	$account = $variables['elements']['#account'];
+  $title = !empty($account->field_title) && !empty($account->field_title[LANGUAGE_NONE][0]['value']) ?
+    $account->field_title[LANGUAGE_NONE][0]['value'] . ' ' : '';
 ?>
 <div class="user_profile clearfix"<?php print $attributes; ?>>
 	<?php print render($user_profile['user_picture']); ?>
 	<?php if (!empty($account->field_full_name)): ?>
 		<div class="field field-name-field-full-name field-type-text field-label-hidden">
 			<div class="field-items">
-				<div class="field-item even"><?php print l($account->field_full_name[LANGUAGE_NONE][0]['value'], 'user/' . $account->uid); ?></div>
+				<div class="field-item even"><?php print l($title . $account->field_full_name[LANGUAGE_NONE][0]['value'], 'user/' . $account->uid); ?></div>
 			</div>
 		</div>
-		<?php hide($user_profile['field_full_name']); ?>
+    <?php hide($user_profile['field_title']); ?>
+    <?php hide($user_profile['field_full_name']); ?>
 	<?php else: ?>
+    <?php print render($user_profile['field_title']); ?>
 		<?php print render($user_profile['field_full_name']); ?>
 	<?php endif; ?>
 	<?php print render($user_profile['field_position_function']); ?>
